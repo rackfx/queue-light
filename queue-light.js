50b6e22e026494144fb983129bea464267f5445e
David R. Jones <david.jones@madwiremedia.com>
2015-10-18 18:43:53 -0600

    scafolding

--- queue-light.js
+++ queue-light.js
@@ -1,28 +1,37 @@
-
-
+var jsonfile = require('jsonfile');
+var util = require('util');
+var _ = require('lodash');
 
 /* exports.init options : {
-
+  queueTimeout : how long items can be in the queue before they're considered "stuck",
+  filename : string
 }
 */
 
 exports.init = function(options){
-  return queueMethods;
+  var result = extend;
+  result.filename = options.filename;
+  result.queueTimeout = options.queueTimeout;
+  return result;
 }
 
 
-var queueMethods = {
+var extend = {
   push: function(data){ // push a new data message into the queue.
+    var json_data = JSON.stringify(data);
+
 
   },
   pull: function(){ // pull's next data message and marks it in progress
 
+    jsonfile.readFile(this.filename, function(err, obj) {
 
+    })
   },
   finish: function(){ // Removes the data message from the queue
 
   },
-  putBack: function(){
+  return: function(){ // Put's a data message back into the queue and mark it "in queue"
 
   }
 }
--- test.js
+++ test.js
@@ -1,6 +1,25 @@
 queuelight = require('./queue-light.js');
-var queue = queuelight.init();
+var queue = queuelight.init(
+  {
+    filename: './data/queue.json',
+    queueTimeout: 0,
+  }
+);
 
 
-
-queue.push();
+var data = {
+  fruits: ['apple','orange','pear'],
+  users: [
+    {
+      name: 'admin',
+    },
+    {
+      name: 'bill'
+    }
+  ]
+}
+queue.push(data);
+queue.pull();
+queue.return();
+queue.pull();
+queue.finish();

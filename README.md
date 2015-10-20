b1f033f6f55fc8ecd6bd23da4dbcae72274f523c
David R. Jones <david.jones@madwiremedia.com>
2015-10-19 19:28:02 -0600

    init

---
+++ README.md
@@ -0,0 +1,75 @@
+# queue-light
+Lighweight file based JSON queuing service.
+
+### Usage:
+
+```bash
+npm install queue-light
+```
+
+```javascript
+// Bring in the module
+var queuelight = require('queue-light');
+
+// Initialize the queue object.
+var queue = queuelight.init(
+  {
+    filename: './data/queue.json',
+    finishedFilename: './data/queue.finished.json',
+    queueTimeout: 0,
+  }
+);
+
+// Set your data
+var data = {name: 'first'}
+
+// Push your data to the json file.
+queue.push(data, function(err,data){
+  console.log(data);
+});
+
+// Pull oldest from the json file
+queue.pull(function(err,item){
+  console.log(item);
+});
+
+// mark the item as finished, this removes it from the json file
+queue.finish(item, function(err, item){
+  console.log(item);
+});
+
+
+```
+### NOTICE
+
+Currently, this module does not create the used data files.  Please create the data files you plan on using and simply put "[]" in for the contents.  TODO: Auto-create files.
+
+### Data Flow
+
+![](https://github.com/rackfx/queue-light/blob/master/queue-light-flow.png?raw=true)
+
+### API
+
+#### .init(options)
+
+Initialize JSON queue file.
+
+##### Options
+- **filename** - (required) The JSON file path for data store
+- **finishedFilename** - (optional) the JSON file path for finished items
+
+#### queue.push(data,cb)
+
+Push new data into the queue, and mark it "in queue" (0).  Returns the entire queue.
+
+#### queue.pull(cb)
+
+Pull the oldest item out of the queue and mark it as "in progress" (1)
+
+#### queue.finish(item,cb)
+
+Remove item from queue.  If finishedFilename is set, the item will be added to the finished items JSON file and marked "Completed" (2)
+
+#### queue.return(item,cb)
+
+Return an item back to the queue, marking it "in queue" (0)
---
+++ queue-light.js
@@ -0,0 +1,148 @@
+var jsonfile = require('jsonfile');
+var util = require('util');
+var _ = require('lodash');
+var moment = require('moment');
+var uuid = require('node-uuid');
+var fs = require('fs');
+
+/* exports.init options : {
+  queueTimeout : how long items can be in the queue before they're considered "stuck",
+  filename : string
+}
+*/
+
+exports.init = function(options){
+  var result = extend;
+  result.filename = options.filename;
+  if(options.finishedFilename) {
+    result.finishedFilename = options.finishedFilename;
+  }
+  result.queueTimeout = options.queueTimeout;
+  var fstat = fs.accessSync(options.filename);
+  console.log(fstat);
+  // TODO: check files to make sure the exist, if not initialize with '[]';
+  return result;
+}
+
+
+var extend = {
+  push: function(data, cb){ // push a new data message into the queue.
+
+    var filename = this.filename;
+    // set defaults before insert
+    data.insertTime = moment().format();
+    data.id = uuid.v1();
+    data.status = 0; // 0 = queued, 1 = in progress, 2 = completed
+
+    jsonfile.readFile(filename, function(err, obj) {
+      obj.push(data);
+      jsonfile.writeFile(filename,obj,function(err){
+        if(err){
+          cb(err);
+        }
+        else {
+          cb(null, obj);
+        }
+      })
+    })
+
+  },
+  pull: function(cb){ // pull's next data message and marks it in progress
+    var filename = this.filename;
+    jsonfile.readFile(filename, function(err, obj) {
+      if(obj.length===0){
+        cb(null, null);
+      }
+      else{
+        var items = _.filter(obj, {'status':0});
+        var sorted = _.sortBy(items, 'insertTime');
+
+        sorted[0].status = 1;
+        sorted[0].pullTime = moment().format();
+
+        jsonfile.writeFile(filename, sorted,function(err){
+            if(err){
+              cb(err);
+            }
+            else {
+              cb(null, sorted[0]);
+            }
+        });
+      }
+
+    })
+  },
+  finish: function(item,cb){ // Removes the data message from the queue
+    var id=item.id;
+    var filename = this.filename;
+    var finishedFilename = "";
+    if (this.finishedFilename){
+      finishedFilename = this.finishedFilename;
+    }
+    jsonfile.readFile(filename, function(err, obj) {
+      if(err){
+        cb(err)
+      }
+      else {
+        var item = _.remove(obj, { 'id':id });
+        item[0].finishedTime = moment().format();
+        item[0].status = 2;
+        jsonfile.writeFile(filename, obj, function(err){
+          if(err){
+            cb(err);
+          }
+          else {
+            if(finishedFilename){
+              jsonfile.readFile(finishedFilename, function(err,finishedItems){
+                if(err){
+                  cb(err)
+                }
+                else {
+
+
+                  finishedItems.push(item[0]);
+
+                  jsonfile.writeFile(finishedFilename, finishedItems, function(err,m){
+                    if(err){
+                      cb(err);
+                    }
+                    else {
+                      cb(null, item);
+                    }
+                  }) // end json writefile
+                }
+              }) // end json readfile
+            } // end if finishedFilename
+            else{
+                cb(null, item);
+            }
+
+          }
+        })
+      }
+
+    });
+  },
+  return: function(item,cb){ // Put's a data message back into the queue and mark it "in queue"
+    var filename = this.filename;
+    jsonfile.readFile(filename, function(err, obj) {
+      if(err){
+        cb(err);
+      }
+      else {
+        var index = _.findIndex(obj, {'id':item.id});
+        obj[index].status = 0;
+        jsonfile.writeFile(filename, obj,function(err){
+            if(err){
+              cb(err);
+            }
+            else {
+              cb(null, obj);
+            }
+        });
+      }
+
+    })
+  },
+
+}

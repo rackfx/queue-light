b6073f721684c6931f07dce5b31d1f7195c24cd1
David R. Jones <drj@rackfx.com>
2015-11-20 21:56:05 -0700

    fix test

--- test/init-push-pull-finish.js
+++ test/init-push-pull-finish.js
@@ -1,7 +1,7 @@
 var assert=require('assert');
 var ql = require('../queue-light');
-var testQueueFile = './test/testqueue.json';
-var testQueueFinishFile = './test/testqueue.finished.json';
+var testQueueFile = './test/testqueue.db';
+//var testQueueFinishFile = './test/testqueue.finished.json';
 var jsonfile = require('jsonfile');
 var fs = require('fs');
 var colors = require('colors')
@@ -9,72 +9,45 @@ var colors = require('colors')
 var queue = ql.init(
   {
     filename: testQueueFile,
-    finishedFilename: testQueueFinishFile,
+
   }
 );
 
 
-describe('queue init',function(){
 
+describe('quite push', function() {
+    it('should should add data to the queue', function(done) {
+        var insertData = {
+            name: "drj",
+            address: '123 main street',
+            tags: ['red', 'green', 'blue']
+        }
 
-  it('should create empty queue files.', function(done){
-    jsonfile.readFile(testQueueFile, function(err, obj) {
-        assert.deepEqual(obj, []);
-        jsonfile.readFile(testQueueFinishFile, function(err,obj){
-          assert.deepEqual(obj, []);
-          done();
-          describe('quite push', function(){
-            it('should complete a data push and store data in queue file',function(done){
-              var insertData = { name: "drj", address: '123 main street', tags: ['red','green','blue']}
-              queue.push(insertData, function(){
-                jsonfile.readFile(testQueueFile, function(err, obj) {
-                  assert.deepEqual(insertData, obj[0].data);
-                  done();
-                  describe('queue pull',function(){
-                    it('should pull the item out of the queue and mark the queue item as "in proress"', function(done){
-                      queue.pull(function(err,item){
-                        if(err){
-                          throw err;
-                        }
-                        else {
-                          // check the item
-                          // check the queue to make sure it's 'in progress'
-                          jsonfile.readFile(testQueueFile, function(err, obj) {
-                            assert.equal(obj[0].status, 1);
-                            assert.deepEqual(insertData, item.data);
-                            done();
-                            describe('finish queue',function(){
-                              it('queue file should be empty array and queue.finish file should contain the object',function(done){
-                                queue.finish(item,function(err,item){
-                                  jsonfile.readFile(testQueueFile, function(err, obj) {
-                                    assert.deepEqual([], obj);
-                                    jsonfile.readFile(testQueueFinishFile, function(err, obj) {
-                                      assert.deepEqual(insertData, obj[0].data)
+        queue.push(insertData, function(err,item) {
 
-                                      done();
-                                    });
-                                  });
-                                });
-                              });
-                            });
-                          });
-                        } // end if
-                      });
-                    });
-                  });
-                });
-              });
+          queue.setReady(item,function(err,item){
+            queue.pull(function(err, item) {
+              console.log(item,err);
+                if (err) {
+                    throw err;
+                } else {
+                    assert.deepEqual(insertData, item.data);
+                    done();
+                }
             });
-          });
+          })
+
+
         });
     });
-  });
 });
+
+
 
 cleanUpOnExit = function(){
   console.log('removing temp queue files'.blue);
   fs.unlink(testQueueFile);
-  fs.unlink(testQueueFinishFile);
+
 }
 process.on('exit', cleanUpOnExit);
 process.on('SIGINT', cleanUpOnExit);
